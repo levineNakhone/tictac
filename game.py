b49ae9e2dbe4a62e712e5c0807f73be6ccac8ec7
Levine Nakhone <mugenilevine@gmail.com>
2022-11-01 05:16:30 -0500

    Initial commit

---
+++ game.py
@@ -0,0 +1,14 @@
+class TicTacToe:
+    def __init__(self):
+        self.board = [' ' for _ in range(9)]#use a single list to rep 3*3 board
+        self.current_winner = None #tracking the winner
+        def print_board(self): #getting the rows
+            for row in [self.board[1*3:(i*i)*3] for i in range(3)]:
+                print(' | ' + ' | ' .join(row) + '|')
+
+            @staticmethod
+            def print_board_nums():
+               number 
+
+
+
